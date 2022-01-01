64339cadbf51c0e503900c6b732176544fe788a0
Rhys Mainwaring <rhys.mainwaring@me.com>
2022-01-01 19:01:38 +0000

    Add check for the api implementation type in extras example
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- extras/python/extras_example.py
+++ extras/python/extras_example.py
@@ -7,10 +7,20 @@ from extras.msgs.vector3d_pb2 import Vector3d
 
 from extras import extras_module as m
 
+from google.protobuf.internal import api_implementation
+
 def main():
   print("Extras Module Example")
   print("===================")
 
+  #----------------------------------------------
+  # check google.protobuf implementation
+  print("\ngoogle.protobuf implementation")
+  print("------------------------------")
+  print(api_implementation.Type())
+
+  #----------------------------------------------
+  # cpp wrapped protobuf
   print("\ncpp vector3d")
   print("------------")
   msg = m.get_vector3d(1.0, 2.0, 3.0)
@@ -18,6 +28,8 @@ def main():
   print("isinstance: {}\n".format(isinstance(msg, Vector3d)))
   msg = m.set_vector3d(msg)
 
+  #----------------------------------------------
+  # native Python protobuf
   print("\npy vector3d")
   print("-----------")
   msg = Vector3d()
